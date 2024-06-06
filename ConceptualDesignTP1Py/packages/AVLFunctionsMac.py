7eb51955411725718b2177876478bf6de81cd3e3
artanisdg <76876320+artanisdg@users.noreply.github.com>
2024-06-06 19:13:37 +0900

    Files not synced properly

---
+++ ConceptualDesignTP1Py/packages/AVLFunctionsMac.py
@@ -0,0 +1,13 @@
+import math
+import subprocess
+
+def RunAVL():0
+
+#avl = subprocess.Popen(["avl.exe"],stdin=subprocess.PIPE, text=True)
+
+
+#print("PLOP",file=avl.stdin)
+#print("G",file=avl.stdin)
+#print(file=avl.stdin)
+#print("Quit",file=avl.stdin)
+
---
+++ ConceptualDesignTP1Py/packages/MainFunctions.py
@@ -0,0 +1,11 @@
+import math
+import sys
+
+if 'darwin' in sys.platform:
+    import packages.AVLFunctionsMac as AVLFunctionsMac
+else:
+    import packages.AVLFunctions as AVLFunctions
+
+def sizing(): 0
+
+def runAVL(): 0
