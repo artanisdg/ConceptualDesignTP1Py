c07d096b98425d9cab55c80d9b7f5af7e37d9472
DongGun Jung <artanisdg@naver.com>
2024-06-07 01:00:15 +0900

    Revert "Developing AVL and acft related functions"
    This reverts commit 8b139c944d9a2846fcd298e73cbed3d345de8468.

--- ConceptualDesignTP1Py/packages/AVLAircraft.py
+++ ConceptualDesignTP1Py/packages/AVLAircraft.py
@@ -9,20 +9,15 @@ if 'darwin' in sys.platform:
 import packages.AVLFunctions as AVLF
 
 
-class Aircraft:
-    def __init__(self,modType,Nm):
+class AircraftData:
+    def __init__(self,modType):
         self.ModelType = modType
-        self.Name = Nm
-        self.CoM = [0,0,0]
         self.Wing()
         self.Fuselage()
         self.HStab()
-        self.VStab()
+        self.Vstab()
         self.MotorPod()
     
-    Name = None
-    Mass = 0
-
     class Wing:
         def __init__(self):
             self.AttachPos = [0,0,0]
@@ -74,56 +69,3 @@ class Aircraft:
         Radius = 0
         Length = 0
         Mass = 0
-        
-    def readFromTxt(self):
-        0 #tbd
-        
-    def CalcCoM(self):
-        GW = 2*self.Wing.Mass+2*self.Fuselage.Mass+2*self.HStab.Mass+self.VStab.Num*self.VStab.Mass+self.MotorPod.Num*self.MotorPod.Mass
-        
-        xM = (2*self.Wing.Mass*(self.Wing.AttachPos[0]+self.Wing.CoM[0])
-             +self.Fuselage.Mass*self.Fuselage.CoM[0]
-             +2*self.HStab.Mass*(self.HStab.AttachPos[0]+self.HStab.CoM[0])
-             +self.VStab.Num*self.VStab.Mass*(self.VStab.AttachPos[0]+self.VStab.CoM[0])
-             +self.MotorPod.Num*self.MotorPod.Mass*(self.MotorPod.AttachPos[0]+self.MotorPod.CoM[0]))
-        x = xM/GW
-
-        yM = self.Fuselage.Mass*self.Fuselage.CoM[1]
-        y = yM/GW
-        
-        zM = (2*self.Wing.Mass*(self.Wing.AttachPos[2]+self.Wing.CoM[2])
-             +self.Fuselage.Mass*self.Fuselage.CoM[2]
-             +2*self.HStab.Mass*(self.HStab.AttachPos[2]+self.HStab.CoM[2])
-             +self.VStab.Num*self.VStab.Mass*(self.VStab.AttachPos[2]+self.VStab.CoM[2])
-             +self.MotorPod.Num*self.MotorPod.Mass*(self.MotorPod.AttachPos[2]+self.MotorPod.CoM[2]))
-        z = zM/GW
-        
-        self.CoM = [x,y,z]
-        self.Mass = GW
-
-        
-
-def WriteACtoFile(Acft : Aircraft,runtm : AVLF.runtime, slices : int):
-    AVLpath = runtm.readAVLFileName()
-    MassPath = runtm.readMassFileName()
-    
-    AVLFile = open(AVLpath,"a")
-    
-    AVLFile.write(Acft.Name)
-    AVLFile.write("")
-    AVLFile.write("#Mach")
-    AVLFile.write(" 0.42")
-    AVLFile.write("")
-    AVLFile.write("#IYsym   IZsym   Zsym")
-    AVLFile.write(" 0       0       0.0")
-    AVLFile.write("")
-    AVLFile.write("#Sref    Cref    Bref")
-    AVLFile.write("1260.0   11.0    113.0")
-    AVLFile.write("")
-    AVLFile.write("#Xref    Yref    Zref")
-    AVLFile.write(Acft.CoM[0],"     ",Acft.CoM[1],"     ",Acft.CoM[2])
-    
-
-    for i in range(1,slices):
-        0 #tbd
-  
--- ConceptualDesignTP1Py/packages/AVLFunctions.py
+++ ConceptualDesignTP1Py/packages/AVLFunctions.py
@@ -1,1 +1,27 @@
-import osimport sysimport mathimport subprocessdef RunAVL():0if 'darwin' in sys.platform:    AVLSession = subprocess.Popen(["/Users/dong-gunjung/Desktop/CDTP1/ConceptualDesignTP1Py/ConceptualDesignTP1Py/avl335"],stdin=subprocess.PIPE, text=True)else:    AVLSession = subprocess.Popen(["avl.exe"],stdin=subprocess.PIPE, text=True)def createAVLFile(name): 0def createMassFile(name): 0class runtime:    def __init__(self,AVLnm,Massnm,Runnm):        self.createRunFile(Runnm)        AVLFileName = AVLnm        MassFileName = Massnm        def createRunFile(self): 0    def modifyRunFile(self,var,value): 0        def readAVLFileName(self):        return self.AVLFileName        def readMassFileName(self):        return self.MassFileName    # print("PLOP",file=avl.stdin)# print("G",file=avl.stdin)# print(file=avl.stdin)# print("Quit",file=avl.stdin)
+import os
+import sys
+import math
+import subprocess
+
+def RunAVL():0
+
+if 'darwin' in sys.platform:
+    AVLSession = subprocess.Popen(["/Users/dong-gunjung/Desktop/CDTP1/ConceptualDesignTP1Py/ConceptualDesignTP1Py/avl335"],stdin=subprocess.PIPE, text=True)
+else:
+    AVLSession = subprocess.Popen(["avl.exe"],stdin=subprocess.PIPE, text=True)
+
+def createAVLFile(name): 0
+
+def createMassFile(name): 0
+
+class runtime:
+    def createRunFile(name): 0
+
+    def modifyRunFile(name,var,value): 0
+
+# print("PLOP",file=avl.stdin)
+# print("G",file=avl.stdin)
+# print(file=avl.stdin)
+# print("Quit",file=avl.stdin)
+
+
