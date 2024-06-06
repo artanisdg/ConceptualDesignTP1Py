de66adf66044a19938d8c8daa5d7e5c665448b07
DongGun Jung <artanisdg@naver.com>
2024-06-06 23:26:51 +0900

    Made all init variables immutable, and used init function for mutables

--- ConceptualDesignTP1Py/packages/AVLAircraft.py
+++ ConceptualDesignTP1Py/packages/AVLAircraft.py
@@ -12,50 +12,60 @@ import packages.AVLFunctions as AVLF
 class AircraftData:
     def __init__(self,modType):
         self.ModelType = modType
+        self.Wing()
+        self.Fuselage()
+        self.HStab()
+        self.Vstab()
+        self.MotorPod()
     
     class Wing:
+        def __init__(self):
+            self.AttachPos = [0,0,0]
+            self.CoM = [0,0,0]
         RootChord = 0
         TipChord = 0
         SpanHalf = 0
         Sweep = 0
         Airfoil = 2412
-        AttachPos = [0,0,0]
-        CoM = [0,0]
         Mass = 0
 
     class Fuselage:
+        def __init__(self):
+            self.CoM = [0,0,0]
         Length = 0
         Height = 0
         SlopeUpper = 0
         SlopeLower = 0
         SlopeSide = 0
-        CoM = [0,0]
         Mass = 0
 
     class HStab:
+        def __init__(self):
+            self.AttachPos = [0,0,0]
+            self.CoM = [0,0,0]
         RootChord = 0
         TipChord = 0
         SpanHalf = 0
         Sweep = 0
         Airfoil = 0
-        AttachPos = [0,0,0]
-        CoM = [0,0]
         Mass = 0
 
     class VStab:
+        def __init__(self):
+            self.AttachPos = [0,0,0]
+            self.CoM = [0,0,0]
         Num = 1
         RootChord = 0
         TipChord = 0
         Span = 0
         Sweep = 0
-        AttachPos = [0,0,0]
-        CoM = [0,0]
         Mass = 0
 
     class MotorPod:
+        def __init__(self):
+            self.AttachPos = [0,0,0]
+            self.CoM = [0,0,0]
         Num = 0
-        AttachPos = [0,0,0]
         Radius = 0
         Length = 0
-        CoM = [0,0]
         Mass = 0
